e015edfb66e2fdf734fd7ecdba73626ad84de3a9
dsikhwal <166606499+dsikhwal@users.noreply.github.com>
2024-10-17 16:48:03 +0000

    Added member.js file

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsmember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['JavaScript', 'React', 'Node', 'MongoDB'],
+        details: {
+            address: '123 Main St',
+            city: 'New York'
+        }
+    };
+    return member;
+}
